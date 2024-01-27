75d14719e9e3c58a6d82c66c96477bfed8d1d263
Outernemesis <outernemesis.00@web.de>
2024-01-27 11:05:08 +0100

    ADD: Folder and basestructure of StageEditor ADD: textmeshpro import

---
+++ Assets/LevelEditor/Scripts/StagePart.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePart
+{
+    public int PlayerId { get; set; }
+    public Dictionary<int, int> stageSets;
+
+}
