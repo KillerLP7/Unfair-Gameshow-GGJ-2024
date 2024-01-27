b95135128f84405bc49d189653fdfd42177c8344
TheDarkRiddle <85240633+TheDarkRiddle@users.noreply.github.com>
2024-01-27 15:26:24 +0100

    Started gamecontroller

---
+++ Assets/Scripts/BaseObstacle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObs : MonoBehaviour
+{
+    private Collider _collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.TryGetComponent(out PlayerController pPayerController)) return;
+        GameManager.Instance.PlayerReset();
+    }
+}
