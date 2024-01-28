b89fad3cfbcd3f8a6332fb143d205ff0c2cbe1fd
Rhyth <rhythns@gmail.com>
2024-01-28 02:08:50 +0100

    Fixed network issues

--- Assets/Scripts/UI/StartScreen/MatchList.cs
+++ Assets/Scripts/UI/StartScreen/MatchList.cs
@@ -1,5 +1,8 @@
 using Mirror;
+using Mirror.SimpleWeb;
 using System.Collections;
+using System.Net;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,11 +19,20 @@ public class MatchList : MonoBehaviour
 
     private void Start()
     {
+        NetManager.singleton.connected.AddListener(OnConnected);
+
 #if UNITY_WEBGL
-        NetworkClient.OnConnectedEvent += OnConnected;
+
+#elif UNITY_SERVER
+        HostServer();
 #else
         HostMatch();
 #endif
+    }
+
+    private void OnDestroy()
+    {
+        NetManager.singleton.connected.RemoveListener(OnConnected);
     }
 
     public void OnTryToConnect()
@@ -32,27 +44,60 @@ public class MatchList : MonoBehaviour
         connectedRoutine = new ExtendedCoroutine(this, Connecting(), startNow: true);
     }
 
-    public void StopTryingToConnect()
+    public void StopTryingToConnect(bool stop = true)
     {
         StopCoroutine(connectedRoutine.Coroutine);
         connectedRoutine = null;
         cancelButton.interactable = true;
 
-        NetManager.singleton.StopClient();
+        if (stop)
+            NetManager.singleton.StopClient();
     }
 
     private void OnConnected()
     {
-        StopTryingToConnect();
+        print("Connected!");
+        StopTryingToConnect(false);
         Destroy(gameObject);
     }
 
     private IEnumerator Connecting()
     {
+        yield return null; // ignore this, dont delete it
+
+        if (!ushort.TryParse(port.text, out ushort castedPort) || !ValidAddress(ip.text))
+        {
+            StopTryingToConnect();
+            yield break;
+        }
+
+        (NetManager.singleton.transport as SimpleWebTransport).port = castedPort;
+        NetManager.singleton.networkAddress = ip.text;
+        NetManager.singleton.StartClient();
+
         yield return new WaitForSeconds(7.0f);
 
-        if (!NetworkClient.isConnected)
-            StopTryingToConnect();
+        StopTryingToConnect();
+    }
+
+    private bool ValidAddress(string address)
+    {
+        // First, try to parse the address as an IP address.
+        if (IPAddress.TryParse(address, out _))
+        {
+            return true; // It's a valid IP address
+        }
+
+        // If it's not an IP address, check if it's a valid hostname
+        // Simple regex for a basic hostname validation (you might need a more complex one depending on your requirements)
+        string hostnameRegex = @"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$";
+        return Regex.IsMatch(address, hostnameRegex);
+    }
+
+    public void HostServer()
+    {
+        NetManager.singleton.StartServer();
+        Destroy(gameObject);
     }
 
     public void HostMatch()
